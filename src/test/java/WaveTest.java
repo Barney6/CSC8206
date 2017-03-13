f14b1dd1ecf416b9bfdae45b9e36e3dae7965392
Shaun Barney <barney_005@hotmail.com>
2017-03-13 18:26:26 +0000

    Wave

--- src/test/java/WaveTest.java
+++ src/test/java/WaveTest.java
@@ -1,3 +1,6 @@
+
+import java.io.IOException;
+
 import org.junit.Test;
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
@@ -8,7 +11,7 @@ import pacemaker.Wave;
 public class WaveTest {
 
 	@Test
-	public void test() {
+	public void test() throws IOException {
 		double[] xData = new double[1000];
 	    double[] yData = new double[1000];
 
@@ -19,8 +22,13 @@ public class WaveTest {
 	    Wave wave = new Wave(200, 120, 320);
 	    yData = wave.generateWave();
 	 
-	    for(int i=0; i<yData.length; i++)
-	    	System.out.println(yData[i]);
+	    // Create Chart
+	    XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+	 
+	    // Show it
+	    new SwingWrapper<XYChart>(chart).displayChart();
+	    
+	    System.in.read();
 	}
 
 }
