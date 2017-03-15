ad6af6d3255a5b2f7d3496032acd225a2b771bbb
Shaun Barney <barney_005@hotmail.com>
2017-03-15 18:25:19 +0000

    summit

--- src/test/java/frameTest.java
+++ src/test/java/frameTest.java
@@ -26,14 +26,14 @@ public class frameTest {
 		
 		LivePlotting swingWorkerRealTime = new LivePlotting("HeartBeat");
 		List<Double> yData = new ArrayList<Double>();
-		Wave wave = new Wave(200, 160);
+		Wave wave = new Wave(100, 160);
 	    yData = wave.generateSlow();
 	    swingWorkerRealTime.chart.setHeartBeat(yData);
 	    swingWorkerRealTime.run();
 	    	    
-	    LivePlotting swingWorkerRealTime1 = new LivePlotting("Pacemake Resource");
+	    LivePlotting swingWorkerRealTime1 = new LivePlotting("Pacemake Response");
 		List<Double> yData1 = new ArrayList<Double>();
-		Wave wave1 = new Wave(200, 160);
+		Wave wave1 = new Wave(100, 160);
 	    yData1 = wave1.generateSlow();
 	    swingWorkerRealTime1.chart.setHeartBeat(yData1);
 	    swingWorkerRealTime1.run();		
